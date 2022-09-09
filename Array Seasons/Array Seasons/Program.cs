f6e6f81fdb220e06b0a660af6baf50d8947545ba
Hellman-P <hellmanp98@gmail.com>
2022-09-09 14:55:48 +0200

    Array seasons and monsters

---
+++ Array Seasons/Array Seasons/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array_Seasons
+{
+    internal class Program
+    {
+        static void CreateDayDescription(int day, int season, int year)
+        {
+            string[] seasons = { "Spring", "Summer", "Autumn", "Winter" };
+
+            Console.WriteLine($"{day}th day of {seasons[season]} in the year {year}");
+
+        }
+        static void Main(string[] args)
+        {
+            CreateDayDescription(5, 0 , 740);
+            CreateDayDescription(7, 1, 740);
+            CreateDayDescription(11, 2, 740);
+            CreateDayDescription(14, 3, 740);
+        }
+    }
+}
---
+++ More monsters/More monsters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace More_monsters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int[] monsterAmount = new int[100];
+
+            Console.Write("Number of monsters in levels: ");
+            for (int i = 0; i < 100; i++)
+            {
+                monsterAmount[i] = random.Next(1, 51);
+            }
+            Array.Sort(monsterAmount);
+            Console.Write(string.Join(", ", monsterAmount));
+            Console.WriteLine();
+        }
+    }
+}
