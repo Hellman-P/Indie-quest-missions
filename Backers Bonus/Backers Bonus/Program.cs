4173b206d3b17aa4a95d0d9021eb53997f3057a0
Hellman-P <hellmanp98@gmail.com>
2022-09-09 13:09:07 +0200

    Backers Bonus

---
+++ Backers Bonus/Backers Bonus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Backers_Bonus
+{
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading backer names
+            string backerNamesPath = "backers.txt";
+            string[] backers = File.ReadAllLines(backerNamesPath);
+
+            // Checking current player
+            Console.WriteLine("State your name traveler.");
+            Console.Write(">");
+            string currentPlayer = Console.ReadLine();
+            Console.WriteLine($"Welcome {currentPlayer}");
+
+            if (backers.Contains(currentPlayer))
+            {
+                Console.WriteLine("You can access cool stuff");
+            }
+            else
+            {
+                Console.WriteLine("You can't access cool stuff");
+            }
+        }
+    }
+}
