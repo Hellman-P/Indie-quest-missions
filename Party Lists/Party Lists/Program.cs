2c33070dbea2a365b2e2afb6a4908a1c4148b596
Hellman-P <hellmanp98@gmail.com>
2022-10-19 16:01:37 +0200

    Travel the world map & party lists

---
+++ Party Lists/Party Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+int Factorial(int n)
+{
+    // Figuring out how many rounds are needed to be fair
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+List<string> ShuffleList(List<string> gamers, List<string> shuffled)
+{
+    // Generating random orders
+    Random random = new Random();
+
+    var gamersCopy = new List<string>(gamers);
+    shuffled.Clear();
+
+    for (int i = 0; i < gamers.Count; i++)
+    {
+        int randomIndex = random.Next(gamersCopy.Count);
+        shuffled.Add(gamersCopy[randomIndex]);
+        gamersCopy.Remove(gamersCopy[randomIndex]);
+    }
+
+    return shuffled;
+}
+//Variabels
+var gamers = new List<string> { "Allie", "Ben", "Claire"};
+var shuffled = new List<string> { };
+int numberOfPlayers = gamers.Count;
+int result = Factorial(numberOfPlayers);
+
+//Displating Result
+Console.WriteLine($"The players who will participate in game are: {string.Join(", ", gamers)} \n");
+Console.WriteLine($"Generating all possible starting positions for {numberOfPlayers} players...\n");
+for (int i = 0; i < result; i++)
+{
+    ShuffleList(gamers, shuffled);
+    Console.WriteLine($"Order of scrubs in session {i+1} are: {string.Join(", ", shuffled)} ");
+}
---
+++ Travel the world map/Travel the world map/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Travel_the_world_map
+{
+    internal class Program
+    {
+        static void ConnectLocations(Location a, Location b)
+        {
+
+        }
+        class Location
+        {
+            public string Name;
+            public string Description;
+            public List<Location> Neighbors = new List<Location>();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var locations = new List<Location>();
+            Location currentLocation = new Location();
+
+            // Destinations
+            locations.Add(new Location // 0
+            {
+                Name = "Winterfell",
+                Description = "the capital of the Kingdom of the North"
+            });
+            locations.Add(new Location // 1
+            {
+                Name = "Riverrun",
+                Description = "a large castle located in the central-western part of the Riverlands"
+            });
+            locations.Add(new Location // 2
+            {
+                Name = "Riverrun",
+                Description = "a large castle located in the central-western part of the Riverlands"
+            });
+            locations.Add(new Location // 3
+            {
+                Name = "The Trident",
+                Description = "one of the largest and most well-known rivers on the continent of Westeros"
+            });
+            locations.Add(new Location // 4
+            {
+                Name = "King's Landing",
+                Description = "the capital, and largest city, of the Seven Kingdoms"
+            });
+            locations.Add(new Location // 5
+            {
+                Name = "Highgarden",
+                Description = "the seat of House Tyrell and the regional capital of the Reach"
+            });
+
+            int randomStart = random.Next(0, locations.Count);
+            currentLocation = locations[randomStart];
+            Console.WriteLine($"You are currently in {currentLocation.Name}, {currentLocation.Description}\n");
+            Console.WriteLine("if you wich to travel your possible destinations are:");
+            Console.WriteLine();
+        }
+    }
+}
