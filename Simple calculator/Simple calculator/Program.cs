0b99d28c6e42aa519cea8ae0e1efcbdc4177ec44
Hellman-P <hellmanp98@gmail.com>
2022-11-07 21:44:20 +0100

    Simple calculator

---
+++ Simple calculator/Simple calculator/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int randomItem = random.Next(5);
+
+                var items = new List<string> { "Your own cleaning robot", "An entire shelf in the fridge", "Two posters of Matej", "Several empty coke cans", "A semi functional xbox controller" };
+
+                Console.WriteLine($"Item: {items[randomItem]}");
+                Console.Write("Set the price of this item: ");
+
+                // Handling player input
+                string input = Console.ReadLine();
+                string[] splitInput = input.Split(' ');
+                int y;
+                int x;
+                int price;
+
+                y = Int16.Parse(splitInput[0]);
+
+                if (splitInput.Length > 1)
+                {
+                    x = Int16.Parse(splitInput[2]);
+
+                    switch (splitInput[1])
+                    {
+                        case "+":
+                            price = y + x;
+                            break;
+
+                        case "-":
+                            price = y - x;
+                            break;
+
+                        case "*":
+                            price = y * x;
+                            break;
+
+                        case "/":
+                            price = y / x;
+                            break;
+
+                        default:
+                            price = y;
+                            break;
+                    }
+
+                }
+                else
+                {
+                    price = y;
+                }
+
+                Console.WriteLine($"Price was set to: {price}$");
+                Console.WriteLine("Price something else? (PRESS ENTER...)");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
